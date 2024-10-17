efa6b01d902d252e09d320ac3711b68380c83e7a
Juan de la cruz <jcruzgarrido@gmail.com>
2024-10-17 13:46:29 +0200

    Add files via upload

---
+++ includes/conexion.php
@@ -0,0 +1,14 @@
+<?php
+// Conexión
+$servidor = 'localhost';
+$usuario = 'name';
+$password = 'password';
+$basededatos = 'database_name';
+$db = mysqli_connect($servidor, $usuario, $password, $basededatos);
+
+mysqli_query($db, "SET NAMES 'utf8'");
+
+// Iniciar la sesión
+if(!isset($_SESSION)){
+	session_start();
+}
